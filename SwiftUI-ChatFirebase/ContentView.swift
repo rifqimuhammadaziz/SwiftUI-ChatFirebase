72b4c9b378472c3a8dc701e2a75b702773cd02c5
Rifqi Muhammad Aziz <IDS.RIFQIM@xl.co.id>
2023-02-23 20:42:36 +0700

    init

---
+++ SwiftUI-ChatFirebase/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  SwiftUI-ChatFirebase
+//
+//  Created by Rifqi Muhammad Aziz on 22/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                Picker(selection: $isLoginMode, label: Text("Picker Here")) {
+                    Text("Login")
+                        .tag(true)
+                    Text("Create Account")
+                        .tag(false)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "person.fill")
+                        .font(.system(size: 64))
+                        .padding()
+                }
+                
+                TextField("Email", text: $email)
+                TextField("Password", text: $password)
+                
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Spacer()
+                        Text("Create Account")
+                            .foregroundColor(.white)
+                            .padding(.vertical, 10)
+                        Spacer()
+                    }
+                    .background(Color.blue)
+                }
+            }
+            .navigationTitle("Create Account")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-ChatFirebase/SwiftUI_ChatFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ChatFirebaseApp.swift
+//  SwiftUI-ChatFirebase
+//
+//  Created by Rifqi Muhammad Aziz on 22/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ChatFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
