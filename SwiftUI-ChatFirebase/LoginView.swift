c012050ebb97195ed797975bd5e88fc8a2378d31
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-02-25 12:56:56 +0700

    create login view

---
+++ SwiftUI-ChatFirebase/LoginView.swift
@@ -0,0 +1,83 @@
+//
+//  ContentView.swift
+//  SwiftUI-ChatFirebase
+//
+//  Created by Rifqi Muhammad Aziz on 22/02/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                
+                VStack(spacing: 16) {
+                    Picker(selection: $isLoginMode, label: Text("Picker Here")) {
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    
+                    if !isLoginMode {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 64))
+                                .padding()
+                        }
+                    }
+                    
+                    Group {
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .textInputAutocapitalization(.never)
+                        
+                        SecureField("Password", text: $password)
+                    }
+                    .padding(12)
+                    .background(.white)
+                    
+                    Button {
+                        handleAction()
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text(isLoginMode ? "Log In" : "Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                                .font(.system(size: 14, weight: .semibold))
+                            Spacer()
+                        }
+                        .background(Color.blue)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle(isLoginMode ? "Login" : "Create Account")
+            .background(Color(.init(white: 0, alpha: 0.05)).ignoresSafeArea())
+        }
+    }
+    
+    private func handleAction() {
+        if isLoginMode {
+            print("Should log into Firebase with existing credentials.")
+        } else {
+            print("Register a new account inside of Firebase Auth & then store the image in Storage.")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
--- SwiftUI-ChatFirebase/SwiftUI_ChatFirebaseApp.swift
+++ SwiftUI-ChatFirebase/SwiftUI_ChatFirebaseApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUI_ChatFirebaseApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
